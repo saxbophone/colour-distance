408d60ad1ca768343d2f5e9587ad2d2359830a21
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-09-28 19:00:55 +0100

    Implemented RGB<->LAB conversions

--- colour-spaces.js
+++ colour-spaces.js
@@ -1,11 +1,121 @@
+// XYZ reference values
+const XYZ_X_REF_VALUE = 95.047;
+const XYZ_Y_REF_VALUE = 100.0;
+const XYZ_Z_REF_VALUE = 108.883;
+
+// XXX: modifies its input!
+function scaleDownRGB(rgb) {
+    rgb[0] /= 255;
+    rgb[1] /= 255;
+    rgb[2] /= 255;
+}
+
+function convertRGBForXYZ(c) {
+    return (c > 0.04045) ? Math.pow((c + 0.055) / 1.055, 2.4) : (c / 12.92);
+}
+
+function RGBToXYZ(rgb) {
+    scaleDownRGB(rgb); // scale down each RGB channel
+    // translate each channel
+    rgb[0] = convertRGBForXYZ(rgb[0]) * 100; // r
+    rgb[1] = convertRGBForXYZ(rgb[1]) * 100; // g
+    rgb[2] = convertRGBForXYZ(rgb[2]) * 100; // b
+    // apply matrix transforms
+    return [
+        rgb[0] * 0.4124 + rgb[1] * 0.3576 + rgb[2] * 0.1805,
+        rgb[0] * 0.2126 + rgb[1] * 0.7152 + rgb[2] * 0.0722,
+        rgb[0] * 0.0193 + rgb[1] * 0.1192 + rgb[2] * 0.9505,
+    ];
+}
+
+function convertXYZForLAB(c) {
+  // converted component needs the cube root of input if over a given size
+  return (c > 0.008856) ? Math.pow(c, (1.0 / 3)) : (7.787 * c) + (16.0 / 116);
+}
+
+function XYZToLAB(xyz) {
+    // skew and convert xyz values
+    xyz[0] = convertXYZForLAB(xyz[0] / XYZ_X_REF_VALUE);
+    xyz[1] = convertXYZForLAB(xyz[1] / XYZ_Y_REF_VALUE);
+    xyz[2] = convertXYZForLAB(xyz[2] / XYZ_Z_REF_VALUE);
+    // convert to LAB ranges
+    return [
+        (116 * xyz[1]) - 16,
+        500 * (xyz[0] - xyz[1]),
+        200 * (xyz[1] - xyz[2]),
+    ];
+}
+
 function RGBToLAB(rgbArr) {
-    // TODO: implement properly
-    return rgbArr.slice();
+    // convert via XYZ as an intermediate
+    // console.log(rgbArr);
+    let xyzArr = RGBToXYZ(rgbArr);
+    let labArr = XYZToLAB(xyzArr);
+    // console.log(labArr);
+    return labArr;
+}
+
+// private helper function for colrcv_lab_to_xyz
+function convertLABForXYZ(c) {
+    // get c cubed
+    let cCubed = Math.pow(c, 3);
+    // converted component depends on size of cubed component
+    return (cCubed > 0.008856) ? cCubed : ((c - 16 / 116.0) / 7.787);
+}
+
+function LABToXYZ(lab) {
+    // skew input values
+    let y = (lab[0] + 16.0) / 116.0;
+    let x = lab[1] / 500.0 + y;
+    let z = y - lab[2] / 200.0;
+    // normalise components and adjust for observer calibration
+    return [
+        XYZ_X_REF_VALUE * convertLABForXYZ(x),
+        XYZ_Y_REF_VALUE * convertLABForXYZ(y),
+        XYZ_Z_REF_VALUE * convertLABForXYZ(z),
+    ];
+}
+
+function convertXYZForRGB(c) {
+    return (c > 0.0031308) ? (1.055 * Math.pow(c, 1 / 2.4) - 0.055) : (12.92 * c);
+}
+
+// XXX: This bit (clamping) wasn't in EasyRGB's algorithm. A bit questionable.
+function clampRGB(rgb) {
+    rgb[0] = (rgb[0] > 255) ? 255 : rgb[0];
+    rgb[0] = (rgb[0] < 0) ? 0 : rgb[0];
+    rgb[1] = (rgb[1] > 255) ? 255 : rgb[1];
+    rgb[1] = (rgb[1] < 0) ? 0 : rgb[1];
+    rgb[2] = (rgb[2] > 255) ? 255 : rgb[2];
+    rgb[2] = (rgb[2] < 0) ? 0 : rgb[2];
+}
+
+// Algorithm: http://www.easyrgb.com/index.php?X=MATH&H=01#text1
+function XYZToRGB(xyz) {
+    // shrink larger numbers downs
+    let x = xyz[0] / 100;
+    let y = xyz[1] / 100;
+    let z = xyz[2] / 100;
+    // multiplex the values
+    let r = x *  3.2406 + y * -1.5372 + z * -0.4986;
+    let g = x * -0.9689 + y *  1.8758 + z *  0.0415;
+    let b = x *  0.0557 + y * -0.2040 + z *  1.0570;
+    // convert components and upscale
+    let rgb = [
+        convertXYZForRGB(r) * 255,
+        convertXYZForRGB(g) * 255,
+        convertXYZForRGB(b) * 255,
+    ];
+    // clamp components
+    clampRGB(rgb);
+    return rgb;
 }
 
 function LABToRGB(labArr) {
-    // TODO: implement properly
-    return labArr.slice();
+    // convert via XYZ as an intermediate
+    let xyzArr = LABToXYZ(labArr);
+    let rgbArr = XYZToRGB(xyzArr);
+    return rgbArr;
 }
 
 class RGB {
@@ -31,12 +141,12 @@ class RGB {
 
     // convert back to CSS hex colour string
     toString() {
-        // convert each channel to string and pad if needed
-        let redString = this.red.toString(16);
+        // cast to int and convert each channel to string and pad if needed
+        let redString = Math.round(this.red).toString(16);
         if (redString.length == 1) { redString = '0' + redString; }
-        let greenString = this.green.toString(16);
+        let greenString = Math.round(this.green).toString(16);
         if (greenString.length == 1) { greenString = '0' + greenString; }
-        let blueString = this.blue.toString(16);
+        let blueString = Math.round(this.blue).toString(16);
         if (blueString.length == 1) { blueString = '0' + blueString; }
         return `#${redString}${greenString}${blueString}`;
     }
